a25dd9d9e11e043957bffd4121e172689c670152
Archit Khode <arkits@outlook.com>
2021-01-20 21:07:00 -0800

    ui: added device connection icon

--- web/src/components/DeviceCards.js
+++ web/src/components/DeviceCards.js
@@ -3,16 +3,46 @@ import { observer } from "mobx-react";
 import { AppStoreContext } from "../store/AppStore";
 import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import WifiIcon from "@material-ui/icons/Wifi";
+import SettingsInputHdmiIcon from "@material-ui/icons/SettingsInputHdmi";
+import PermScanWifiIcon from "@material-ui/icons/PermScanWifi";
 
 function DeviceCard({ device }) {
+  const renderConnectionIcon = (connectionType) => {
+    if (connectionType === "WIRELESS") {
+      return <WifiIcon fontSize="large" />;
+    } else if (connectionType === "WIRED") {
+      return <SettingsInputHdmiIcon fontSize="large" />;
+    } else {
+      return <PermScanWifiIcon fontSize="large" />;
+    }
+  };
+
   return (
     <div style={{ paddingBottom: "10px" }}>
       <Card>
         <CardContent>
-          <Typography color="textSecondary">{device?.id}</Typography>
+          <Grid container spacing="3">
+            <Grid item xs={2}>
+              <center>
+                {renderConnectionIcon(device?.connectionType)}
+                <Typography variant="subtitle1" color="textPrimary">
+                  {device?.connectionType}
+                </Typography>
+              </center>
+            </Grid>
+            <Grid item xs={10}>
+              <Typography variant="subtitle1" color="textPrimary">
+                {device?.friendlyName}
+              </Typography>
+              <Typography variant="body2" color="textSecondary">
+                {device?.id}
+              </Typography>
+              <pre>{JSON.stringify(device, null, 4)}</pre>
+            </Grid>
+          </Grid>
         </CardContent>
       </Card>
     </div>
@@ -34,9 +64,7 @@ const DeviceCardListing = observer(({}) => {
   return (
     <div>
       <div style={{ paddingBottom: "20px" }}>
-        <center>
-          <Typography variant="overline">Connected Devices</Typography>
-        </center>
+        <Typography variant="h6">Connected Devices</Typography>
       </div>
       <DeviceCards devices={appStore.devices} />
     </div>
--- web/src/components/StatusBanner.js
+++ web/src/components/StatusBanner.js
@@ -6,12 +6,22 @@ import Typography from "@material-ui/core/Typography";
 const StatusBanner = observer(({}) => {
   const appStore = useContext(AppStoreContext);
 
+  const getNumberOfConnectedDevice = (devices) => {
+    let numberOfConnectedDevices = 0;
+    devices.forEach((device) => {
+      if (device?.ipAddresses !== null) {
+        numberOfConnectedDevices++;
+      }
+    });
+    return numberOfConnectedDevices;
+  };
+
   return (
     <div style={{ marginTop: "5%", padding: "5%" }}>
       <center>
-        <Typography variant="h3">Network is online</Typography>
+        <Typography variant="h3">HashMap is online</Typography>
         <Typography variant="h5">
-          {appStore.devices.length} devices connected
+          {getNumberOfConnectedDevice(appStore.devices)} devices connected
         </Typography>
       </center>
     </div>
