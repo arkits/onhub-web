b4f031f66a845000ae0f3eb0c1c82e85a917b00c
Archit Khode <archit.khode@gmail.com>
2021-01-24 22:29:40 -0800

    ui: render cumulative speeds in the StatusBanner

--- web/src/components/StatusBanner.js
+++ web/src/components/StatusBanner.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { observer } from "mobx-react";
 import { AppStoreContext } from "../store/AppStore";
 import Typography from "@material-ui/core/Typography";
+import { formatBytes } from "../utils/utils";
 
 const StatusBanner = observer(({}) => {
   const appStore = useContext(AppStoreContext);
@@ -16,12 +17,26 @@ const StatusBanner = observer(({}) => {
     return numberOfConnectedDevices;
   };
 
+  const renderNetworkMetrics = (networkMetrics) => {
+    if (networkMetrics) {
+      return `${formatBytes(
+        networkMetrics[0]?.network_metrics?.groupTraffic?.transmitSpeedBps
+      )} upload - ${formatBytes(
+        networkMetrics[0]?.network_metrics?.groupTraffic?.receiveSpeedBps
+      )} download`;
+    } else {
+      return null;
+    }
+  };
+
   return (
     <div style={{ marginTop: "5%", padding: "5%" }}>
       <center>
         <Typography variant="h3">HashMap is online</Typography>
         <Typography variant="h5">
-          {getNumberOfConnectedDevice(appStore.devices)} devices connected
+          {getNumberOfConnectedDevice(appStore.devices)} devices connected{" "}
+          <br></br>
+          {renderNetworkMetrics(appStore.networkMetrics.data)}
         </Typography>
       </center>
     </div>
